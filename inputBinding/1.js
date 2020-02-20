bf4eef45809bc437fdaf60fabba7b862d73dc67f
karoreno <reno@cybergl.co.id>
2020-02-21 00:32:23 +0700

    Create 1.js

---
+++ inputBinding/1.js
@@ -0,0 +1,12 @@
+<script>
+    var app = new Vue({
+        el: "#app",
+        data: {
+            pesan_input: '',
+            pesan_textarea: '',
+            msg: '',
+            age: '',
+            pesan: ''
+        },
+    })
+</script>
