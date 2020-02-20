89649a59681329ed86da23812d979ef88784cfaa
karoreno <reno@cybergl.co.id>
2020-02-20 23:46:18 +0700

    Create 1.js

---
+++ simpleForm/1.js
@@ -0,0 +1,22 @@
+new Vue({
+  el: '#app',
+  data: {
+    form: {
+      formName: 'Tell Us About Yourself',
+      userName: '',
+      favoriteColor: 'Red',
+      favoriteHamburger: '',
+      favoriteHangout: [],
+      workHours: 0
+    },
+    showSubmitFeedback: false
+  },
+  methods: {
+    fakeSubmit(){
+      this.showSubmitFeedback = true;
+      setTimeout(() => {
+        this.showSubmitFeedback = false;
+      }, 3000);
+    }
+  }
+});
